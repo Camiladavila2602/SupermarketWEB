4de5cba9468bf12850db20b89d92aba37cf8aa42
Camiladavila2602 <camiladavila2602@gmail.com>
2023-11-17 10:21:54 -0500

    Edit Products

---
+++ Pages/Products/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarkerWEB.Data;
+using SupermarkerWEB.Models;
+
+namespace SupermarkerWEB.Pages.Products
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public IndexModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Products { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Products != null)
+            {
+                Products = await _context.Products.Include(p => p.Category).ToListAsync();
+            }
+        }
+    }
+}
