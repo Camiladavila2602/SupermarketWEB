a1d88e877db11952a32801566a0f65775d156210
Camiladavila2602 <camiladavila2602@gmail.com>
2023-11-17 12:10:35 -0500

    Index Providers

--- Data/SupermarketContext.cs
+++ Data/SupermarketContext.cs
@@ -12,6 +12,7 @@ namespace SupermarkerWEB.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<PayMode> PayMode { get; set; }
+        public DbSet<Provider> Provider { get; set; }
 
     }
 }
